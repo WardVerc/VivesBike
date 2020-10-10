79e602d1d7cd8f9774bbb010006ec9d6baac19b6
Katrien Deleu <katrien.deleu@vives.be>
2020-10-10 11:49:27 +0200

    serverTimezone aangepast

--- src/main/java/be/vives/ti/dao/connect/ConnectionManager.java
+++ src/main/java/be/vives/ti/dao/connect/ConnectionManager.java
@@ -8,21 +8,20 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 
-    /**
-     * Levert een connectie naar de be.vives.DAO. Leest hiervoor uit het bestand
-     * DB.properties
-     *
-     * @return connectie-object naar de be.vives.DAO
-     * @throws DBException wanneer de be.vives.DAO niet toegankelijk is
-     */
-    public static Connection getConnection() throws DBException {
+	/**
+	 * Levert een connectie naar de be.vives.DAO. Leest hiervoor uit het bestand
+	 * DB.properties
+	 *
+	 * @return connectie-object naar de be.vives.DAO
+	 * @throws DBException wanneer de be.vives.DAO niet toegankelijk is
+	 */
+	public static Connection getConnection() throws DBException {
 
-        try {
-            //driver laden
-            Class.forName(DBProp.getDriver());
-            return DriverManager.getConnection(DBProp.getDbUrl(), DBProp.getLogin(), DBProp.getPaswoord());
-        } catch (ClassNotFoundException | SQLException ex) {
-            throw new DBException("Connectie met de be.vives.DAO mislukt: " + ex);
-        }
-    }
+		try {
+			Class.forName(DBProp.getDriver());
+			return DriverManager.getConnection(DBProp.getDbUrl(), DBProp.getProp());
+		} catch (ClassNotFoundException | SQLException ex) {
+			throw new DBException("Connectie met de be.vives.DAO mislukt: " + ex);
+		}
+	}
 }
--- src/main/java/be/vives/ti/dao/connect/DBProp.java
+++ src/main/java/be/vives/ti/dao/connect/DBProp.java
@@ -6,72 +6,59 @@ import java.util.Properties;
 
 public class DBProp {
 
-    private static String dbUrl;
-    private static String driver;
-    private static String login;
-    private static String paswoord;
+	private static String dbUrl;
+	private static Properties prop;
+	private static String driver;
 
-    /**
-     * Haalt de URL, driver paswoord en login uit het bestand DB.properties en
-     * vult deze in in de overeenkomstige velden
-     */
-    private DBProp() throws DBException {
-        Properties appProperties = new Properties();
-        try {
-            appProperties.load(this.getClass().getResourceAsStream(
-                    "/database/DB.properties"));
-            dbUrl = appProperties.getProperty("dbUrl");
-            driver = appProperties.getProperty("driver");
-            login = appProperties.getProperty("login");
-            paswoord = appProperties.getProperty("paswoord");
+	/**
+	 * Haalt de URL, driver paswoord en login uit het bestand DB.properties en
+	 * vult deze in in de overeenkomstige velden
+	 */
+	private DBProp() throws DBException {
+		prop = new Properties();
+		try {
+			prop.load(this.getClass().getResourceAsStream(
+					"/database/DB.properties"));
+			dbUrl = prop.getProperty("dbUrl");
+			driver = prop.getProperty("driver");
+			prop.setProperty("serverTimezone", java.util.TimeZone.getDefault().getID());
 
-        } catch (java.io.IOException ex) {
-            throw new DBException(
-                    "Bestand (DB.properties) met gegevens over DB niet gevonden.");
-        }
-    }
+		} catch (java.io.IOException ex) {
+			throw new DBException(
+					"Bestand (DB.properties) met gegevens over DB niet gevonden.");
+		}
+	}
 
-    /**
-     * @return the dbUrl
-     * @throws DBException wanneer DB.properties niet toegankelijk is
-     */
-    public static String getDbUrl() throws DBException {
-        if (dbUrl == null) {
-            DBProp db = new DBProp();
-        }
-        return dbUrl;
-    }
+	/**
+	 * @return the dbUrl
+	 * @throws DBException wanneer DB.properties niet toegankelijk is
+	 */
+	public static String getDbUrl() throws DBException {
+		if (dbUrl == null) {
+			DBProp db = new DBProp();
+		}
+		return dbUrl;
+	}
 
-    /**
-     * @return the driver
-     * @throws DBException wanneer DB.properties niet toegankelijk is
-     */
-    public static String getDriver() throws DBException {
-        if (driver == null) {
-            DBProp db = new DBProp();
-        }
-        return driver;
-    }
+	/**
+	 * @return the driver
+	 * @throws DBException wanneer DB.properties niet toegankelijk is
+	 */
+	public static Properties getProp() throws DBException {
+		if (prop == null) {
+			DBProp db = new DBProp();
+		}
+		return prop;
+	}
 
-    /**
-     * @return the login
-     * @throws DBException wanneer DB.properties niet toegankelijk is
-     */
-    public static String getLogin() throws DBException {
-        if (login == null) {
-            DBProp db = new DBProp();
-        }
-        return login;
-    }
-
-    /**
-     * @return the paswoord
-     * @throws DBException wanneer DB.properties niet toegankelijk is
-     */
-    public static String getPaswoord() throws DBException {
-        if (paswoord == null) {
-            DBProp db = new DBProp();
-        }
-        return paswoord;
-    }
+	/**
+	 * @return the login
+	 * @throws DBException wanneer DB.properties niet toegankelijk is
+	 */
+	public static String getDriver() throws DBException {
+		if (driver == null) {
+			DBProp db = new DBProp();
+		}
+		return driver;
+	}
 }
